67e03db784e699bfd386a450ce934a2fb73c7085
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-21 20:10:12 -0400

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import { useCounter } from "./store";
 
 function App() {
   const count = useCounter((state) => state.count);
+  const { increment, decrement } = useCounter((state) => state);
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center text-xl font-bold">
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center text-xl p-5 font-bold">
       {count}
+      <div className="flex flex-col m-2 gap-2">
+        <button className="flex p-2 border-2" onClick={increment}>
+          Increment
+        </button>
+        <button className="flex p-2 border-2" onClick={decrement}>
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
--- src/store.ts
+++ src/store.ts
@@ -2,13 +2,13 @@ import { create } from "zustand";
 
 type CounterStore = {
   count: number;
-  incerment: () => void;
+  increment: () => void;
   decrement: () => void;
 };
 
 export const useCounter = create<CounterStore>((set) => ({
   count: 0,
 
-  incerment: () => set((state) => ({ count: state.count + 1 })),
+  increment: () => set((state) => ({ count: state.count + 1 })),
   decrement: () => set((state) => ({ count: state.count - 1 })),
 }));
