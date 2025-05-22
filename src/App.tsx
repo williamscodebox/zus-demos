14f8382a87c3fd50ef4220d2b37f501081c68f91
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-22 00:46:00 -0400

    setup for new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import PasswordGenerater from "./components/PasswordGenerater";
-
 function App() {
-  return (
-    <div className=" min-h-screen bg-gray-50 flex items-center justify-center">
-      <PasswordGenerater />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
---
+++ src/storage/passwordStore/App.tsx
@@ -0,0 +1,11 @@
+// import PasswordGenerater from "./components/PasswordGenerater";
+
+// function App() {
+//   return (
+//     <div className=" min-h-screen bg-gray-50 flex items-center justify-center">
+//       <PasswordGenerater />
+//     </div>
+//   );
+// }
+
+// export default App;
