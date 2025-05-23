c00c20633366943a081933d5293afdee13cc2799
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-22 22:26:16 -0400

    added useFetch hook

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import TodoList from "./components/TodoList";
 import Authorized from "./components/Authorized";
 import withAccessControl from "./utils/withAccessControl";
 import NotAuthorized from "./components/NotAuthorized";
+import useFetch from "./hooks/useFetch";
 
 const MyComponentWithLoading = withLoading(DataComponent);
 const TodoListWrapper = printProps(Test);
@@ -15,8 +16,11 @@ const TodoListWrap = withTodo(TodoList, "2");
 const MyComponentWithAccessControl = withAccessControl(Authorized);
 
 function App() {
+  const { data, loading, error } = useFetch<any>(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState("");
+  const [data2, setData] = useState("");
 
   useEffect(() => {
     setTimeout(() => {
@@ -29,7 +33,7 @@ function App() {
     <div>
       <TodoListWrapper one={1} two={"hello"} />
       <div>
-        <MyComponentWithLoading isLoading={isLoading} data={data} />
+        <MyComponentWithLoading isLoading={isLoading} data={data2} />
       </div>
       <div>
         <TodoListWrap />
@@ -45,6 +49,12 @@ function App() {
             userPermissions: ["view_dashboard", "edit_settings"],
           }}
         />
+      </div>
+      <div className="mb-4">
+        <h2 className="text-xl">Fetch Data Example</h2>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: {error}</p>}
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
       </div>
     </div>
   );
